58b0e3de1314e178a976fea8ec39531969a5ec41
tokura <2026007@kyoei.co.jp>
2026-04-23 09:53:14 +0900

    基礎練習問題

---
+++ Java_training/src/question12/Dog.java
@@ -0,0 +1,8 @@
+package question12;
+
+public class Dog {
+	public void showName(String n) {
+		System.out.println("名前は" + n + "です");
+	}
+
+}
---
+++ Java_training/src/question12/Question12_01.java
@@ -0,0 +1,14 @@
+package question12;
+
+public class Question12_01 {
+
+	public static void main(String[] args) {
+		Dog name = new Dog();
+		
+		
+		name.showName  ("ダニエル");
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
