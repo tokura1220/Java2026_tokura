2555d1f6c2a1d9ad29a3cbfd7098523459418f86
tokura <2026007@kyoei.co.jp>
2026-05-12 13:53:52 +0900

    Question03_02 提出

---
+++ C/c_training/Question03_02/Question03_02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+
+	printf("15Å{6 ÇÕ %d Ç≈Ç∑ \n",15 + 6);
+	printf("15Å|6 ÇÕ %d Ç≈Ç∑ \n",15-6);
+	printf("15Å~6 ÇÕ %d Ç≈Ç∑\n",15*6);
+	printf("15ÅÄ6 ÇÕ %d Ç≈Ç∑\n",15/6);
+	printf("15ÅÄ6 ÇÃó]ÇËÇÕ %d Ç≈Ç∑\n",15%6);
+}
