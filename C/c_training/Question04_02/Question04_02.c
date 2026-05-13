6752c0d96463889c36a37b5dff5d107b45829505
tokura <2026007@kyoei.co.jp>
2026-05-13 09:49:12 +0900

    練習問題Question04_02 提出

--- C/c_training/Question04_02/Question04_02.c
+++ C/c_training/Question04_02/Question04_02.c
@@ -3,9 +3,12 @@
 int main (void)
 {
 	printf("処理を開始します。\n");
-	int iNumber = 10;
-	if(iNumber == 10){
-		printf("iNumber の値は %d 以上です。 \n",iNumber);
+	int iNumber = 9;
+	if(iNumber >= 10){
+		printf("iNumber の値は %d 以上です。 \n",10);
+	}
+	else {
+		printf("iNumber の値は %d 未満です。 \n",10);
 	}
 	printf("\n処理を終了します。 ");
 	return 0;
