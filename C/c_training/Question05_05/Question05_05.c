5307363e90a918fab464f8567906fffd299355b5
tokura <2026007@kyoei.co.jp>
2026-05-13 11:59:20 +0900

    練習問題Question05_05 提出

---
+++ C/c_training/Question05_05/Question05_05.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+int main(void) {
+	for (int i = 1; i <= 10; i++) {
+		// ----C³‘ÎÛ‚±‚±‚©‚ç---- 
+
+		if(i % 2 ==0 ){
+			printf("%d ‰ñ–Ú‚Ìˆ—‚Å‚·\n", i);
+
+		}
+
+		//printf("%d ‰ñ–Ú‚Ìˆ—‚Å‚·\n", i);
+
+		// ----C³‘ÎÛ‚±‚±‚Ü‚Å---- 
+	}
+
+	printf("ˆ—‚ðI—¹‚µ‚Ü‚·");
+
+	return 0;
+}
