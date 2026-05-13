f829a18d08dde5467f7747b95c116b72eb823f70
tokura <2026007@kyoei.co.jp>
2026-05-13 13:15:22 +0900

    練習問題Question05_07提出

---
+++ C/c_training/Question05_07/Question05_07.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	printf("‹ã‹ã‚ð•\Ž¦‚µ‚Ü‚· \n");
+	for (int i = 1; i < 10; i++) {
+		printf("%d‚Ì’i",i);
+
+		for (int j = 1; j < 10; j++) {
+			printf("%4d ", i * j);
+		}
+		printf("\n");
+	}
+
+}
