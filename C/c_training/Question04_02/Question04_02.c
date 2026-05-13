5df12755fefdfc437a873e087862c4f93e78dcd3
tokura <2026007@kyoei.co.jp>
2026-05-13 09:45:02 +0900

    練習問題Question04_01提出

---
+++ C/c_training/Question04_02/Question04_02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+	printf("処理を開始します。\n");
+	int iNumber = 10;
+	if(iNumber == 10){
+		printf("iNumber の値は %d 以上です。 \n",iNumber);
+	}
+	printf("\n処理を終了します。 ");
+	return 0;
+
+}
